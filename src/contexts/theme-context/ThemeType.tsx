7b33418029cda189668134596e944c0ca563a75d
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-13 16:44:21 +0800

    add more themes

--- src/contexts/theme-context/ThemeType.tsx
+++ src/contexts/theme-context/ThemeType.tsx
@@ -1,6 +1,19 @@
+export enum ThemeEnum {
+  DARK_ORANGE = 'theme-dark-orange',
+  RED = 'theme-red',
+  EMERALD = 'theme-emerald',
+  PASTEL =  'theme-pastel',
+  NEON = 'theme-neon',
+  GOLD = 'theme-gold',
+  VINTAGE = 'theme-vintage',
+  DARK = 'theme-dark',
+  COLD = 'theme-cold',
+  SAKURA = 'theme-sakura',
+  TEST = 'theme-test'
+}
+
 export type ThemeType = {
   theme?: string
-  defaultTheme?: () => string
-  redTheme?: () => string
-  emeraldTheme?: () => string
+  updateTheme: (theme: string) => void
+  restoreDefault: () => void
 }
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,20 +1,29 @@
 import { ThemeButton } from '../components/buttons/Button'
 import Toolbar from '../components/Toolbar'
 import { useTheme } from '../contexts/theme-context/ThemeContext'
+import { ThemeEnum } from "../contexts/theme-context/ThemeType"
 
 type Props = {}
 
 const Settings = (props: Props) => {
-  const {defaultTheme, redTheme, emeraldTheme} = useTheme()
+  const {restoreDefault, updateTheme} = useTheme()
   return (
     <>
       <Toolbar pageName='Settings'/>
       <div className='flex flex-row items-center gap-2'>
         <header className='font-semibold'>Theme Selector:</header>
-        <div className='flex gap-2'>
-          <ThemeButton handleClick={defaultTheme} className={'theme-dark-orange'}/>
-          <ThemeButton handleClick={redTheme} className={'theme-red'}/>
-          <ThemeButton handleClick={emeraldTheme} className={'theme-emerald'}/>
+        <div className='flex flex-wrap gap-2'>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.DARK_ORANGE)} className={ThemeEnum.DARK_ORANGE}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.RED)} className={ThemeEnum.RED}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.EMERALD)} className={ThemeEnum.EMERALD}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.PASTEL)} className={ThemeEnum.PASTEL}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.NEON)} className={ThemeEnum.NEON}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.GOLD)} className={ThemeEnum.GOLD}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.VINTAGE)} className={ThemeEnum.VINTAGE}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.DARK)} className={ThemeEnum.DARK}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.COLD)} className={ThemeEnum.COLD}/>
+          <ThemeButton handleClick={() => updateTheme(ThemeEnum.SAKURA)} className={ThemeEnum.SAKURA}/>
+          {/* <ThemeButton handleClick={() => updateTheme('theme-test')} className={'theme-test'}/> */}
         </div>
       </div>
     </>
