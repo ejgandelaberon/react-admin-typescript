5314bfe44336dc0d166e90a572188e5a10f4640b
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-13 16:44:32 +0800

    remove border

--- src/layouts/Content.tsx
+++ src/layouts/Content.tsx
@@ -8,7 +8,7 @@ type Props = {}
 const Content = (props: Props) => {
   return (
     <div className="fixed h-[calc(100vh-72px)] w-[calc(100vw-240px)] right-0 bottom-0">
-      <div className="m-8 p-8 h-[calc(100%-64px)] w-[calc(100%-62px)] border-dashed border-[10px] border-nav-main rounded-lg">
+      <div className="m-4 p-4 h-[calc(100%-64px)] w-[calc(100%-62px)] rounded-lg">
         <Routes>
           <Route path='home' element={<Home/>}/>
           <Route path='reports' element={<Reports/>}/>
